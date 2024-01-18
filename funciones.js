1b48be02b5642b1b6a39bd079a6e920054690fbc
Pablo Arellano <147171908+pablo22a@users.noreply.github.com>
2024-01-18 13:13:08 -0700

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -15,12 +15,56 @@ function colocarReina(celda) {
       for (let i = 0; i < 8; i++) {
         if (columna != i) {
           tablero.rows[renglon].cells[i].removeAttribute("onclick");
+          tablero.rows[renglon].cells[i].style.backgroundColor="#ff0000";
         }
         if (renglon != i) {
           tablero.rows[i].cells[columna].removeAttribute("onclick");
+          tablero.rows[i].cells[columna].style.backgroundColor="#ff0000";
         }
       }
+      /* Recorremos diagonales */
 
+      var r = renglon;
+      var c = columna;
+
+      while (r >= 0 && c < 8) {
+        
+        tablero.rows[r].cells[c].style.backgroundColor="#ff0000";
+        r--;
+        c++;
+      }
+
+      r = renglon + 1;
+      c = columna - 1;
+
+      while (c >= 0 && r < 8) {
+        
+        tablero.rows[r].cells[c].style.backgroundColor="#ff0000";
+        r++;
+        c--;
+      }
+
+      /* Diagonal superior izquierda */
+
+      r = renglon;
+      c = columna;
+
+      while (c >= 0 && r < 8) {
+        
+        tablero.rows[r].cells[c].style.backgroundColor="#ff0000";
+        r++;
+        c++;
+      }
+
+      r = renglon;
+      c = columna;
+
+      while (r >= 0 && c < 8) {
+        
+        tablero.rows[r].cells[c].style.backgroundColor="#ff0000";
+        r--;
+        c--;
+      }
       
 
       reinasPorColocar--;
@@ -35,7 +79,9 @@ function colocarReina(celda) {
       alert(renglon + " " + columna);
       for (let i = 0; i < 8; i++) {
         tablero.rows[renglon].cells[i].setAttribute("onclick", "colocarReina(this)");
+        tablero.rows[renglon].cells[i].style.backgroundColor="";
         tablero.rows[i].cells[columna].setAttribute("onclick", "colocarReina(this)");
+        tablero.rows[i].cells[columna].style.backgroundColor="";
       }
     reinasPorColocar++;
     reinasColocadas--;
